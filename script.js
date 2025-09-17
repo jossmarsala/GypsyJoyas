a0395391969b71a116a3c266efeb7b20c3397c85
Josefina <marsalahjosefina@gmail.com>
2025-09-16 21:44:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener('load', () => {
 });
 
 // Aumentar precios según porcentaje
-const incremento = 0.05;
+const incremento = 0.06;
 
 document.addEventListener("DOMContentLoaded", function () {
 const precios = document.querySelectorAll(".precio");
@@ -393,6 +393,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.appendChild(overlay);
     }
 });
+
 
 
 
